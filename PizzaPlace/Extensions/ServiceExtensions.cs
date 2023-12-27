449a066cddc8d92a548cc5fae49fb002109cbbd8
fgregec <frangregec2110@gmail.com>
2023-12-27 19:45:50 +0100

    Refactoring

---
+++ PizzaPlace/Extensions/ServiceExtensions.cs
@@ -0,0 +1,75 @@
+﻿using Core.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Decorators;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using PizzaPlace.Helpers;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace PizzaPlace.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddMyServices(this IServiceCollection services, IConfiguration config)
+        {
+
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaPlace", Version = "v1" });
+            });
+
+            services.AddControllers();
+            services.AddScoped<IGuestRepository, GuestRepository>();
+
+            services.AddScoped<IGuestOrderRepository, GuestOrderRepository>();
+            services.Decorate<IGuestOrderRepository, CachingGuestOrderRepository>();
+
+
+            services.AddScoped<IDevelopmentRepository, DevelopmentRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+            services.AddSignalR();
+
+            services.AddAutoMapper(typeof(MappingProfiles));
+
+            // AUTH
+            var key = Encoding.ASCII.GetBytes("very_secret_key!!!!!");
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Employee", policy => policy.RequireClaim(ClaimTypes.Role, "CHEF", "WAITER"));
+                options.AddPolicy("Manager", policy => policy.RequireClaim(ClaimTypes.Role, "MANAGER"));
+            });
+
+            services.AddMemoryCache();
+
+            return services;
+        }
+    }
+}
--- PizzaPlace/Program.cs
+++ PizzaPlace/Program.cs
@@ -1,71 +1,12 @@
-using Core.Interfaces;
-using Infrastructure.Data;
-using Infrastructure.Decorators;
-using Infrastructure.Repositories;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using PizzaPlace;
-using PizzaPlace.Helpers;
+using PizzaPlace.Extensions;
 using Stripe;
-using System.Security.Claims;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 StripeConfiguration.ApiKey = "sk_test_51MVytIFqBUpBtecpwWeBDmvkFs3nGbEaeDS1qZYxWVYpmCPblHxCojWT5vpoIuI8qgN2wno0yLgBHJ9shXBj4qRn00gkQdko8n";
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaPlace", Version = "v1" });
-});
-
-builder.Services.AddControllers();
-builder.Services.AddScoped<IGuestRepository, GuestRepository>();
-
-builder.Services.AddScoped<IGuestOrderRepository, GuestOrderRepository>();
-builder.Services.Decorate<IGuestOrderRepository, CachingGuestOrderRepository>();
-
-
-builder.Services.AddScoped<IDevelopmentRepository, DevelopmentRepository>();
-builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-
-builder.Services.AddSignalR();
-
-builder.Services.AddAutoMapper(typeof(MappingProfiles));
-
-// AUTH
-var key = Encoding.ASCII.GetBytes("very_secret_key!!!!!"); 
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = false;
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
-
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("Employee", policy => policy.RequireClaim(ClaimTypes.Role, "CHEF", "WAITER"));
-    options.AddPolicy("Manager", policy => policy.RequireClaim(ClaimTypes.Role, "MANAGER"));
-});
-
-builder.Services.AddMemoryCache();
+builder.Services.AddMyServices(builder.Configuration);
 
 var app = builder.Build();
 
